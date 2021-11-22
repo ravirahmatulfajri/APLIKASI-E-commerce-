ffe3db3911b9443fd28433b59b6edfcb2da82521
ravirahmatulfajri <47803082+ravirahmatulfajri@users.noreply.github.com>
2021-11-22 10:33:12 +0700

    Delete user directory

--- user/config/fungsi_badword.php
+++
@@ -1,9 +0,0 @@
-<?php
-function sensor($teks){
-    $w = mysqli_query($teks,"SELECT * FROM katajelek");
-    while ($r = mysqli_fetch_array($w)){
-        $teks = str_ireplace($r['kata'], $r['ganti'], $teks);       
-    }
-    return $teks;
-}  
-?>
--- user/dina_meta2.php
+++
@@ -1,5 +0,0 @@
-<?php
-      $sql2 = mysqli_query($connect,"select meta_keyword from modul where id_modul='43'");
-      $j2   = mysqli_fetch_array($sql2);
-		  echo "$j2[meta_keyword]";
-?>
