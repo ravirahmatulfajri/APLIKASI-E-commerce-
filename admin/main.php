dee9675676573d6020ee99121899c1a7dcf42510
dinar <1910631170157@student.unsika.ac.id>
2021-12-09 12:05:23 +0700

    ubah format penulisan deskripsi

--- admin/main.php
+++ admin/main.php
@@ -90,8 +90,7 @@ if (empty($_SESSION['username']) && empty($_SESSION['password'])) {
         include "template/footer.php";
 
     } elseif ($_SESSION['level'] == 'operator') {
-        include "template/header.php";
-        include "template/sidebar_operator.php";
+        include "template/header_op.php";
 
         if (empty($_GET)) {
             include "pages/home/home.php";
@@ -100,44 +99,25 @@ if (empty($_SESSION['username']) && empty($_SESSION['password'])) {
 
         } else if ($_GET['pages'] == 'testi') {
             include "pages/testi/testi.php";
+        } else if ($_GET['pages'] == 'aksi_testi') {
+            include "pages/testi/aksi.php";
 
-        } else if ($_GET['pages'] == 'laporan_produk') {
-            include "pages/guest/produk.php";
-        } else if ($_GET['pages'] == 'laporan_order') {
-            include "pages/guest/order.php";
+        } else if ($_GET['pages'] == 'laporan') {
+            include "pages/laporan/laporan.php";
 
-        } else if ($_GET['pages'] == 'account') {
-            include "pages/account/account_page.php";
-        } else if ($_GET['pages'] == 'detail_account') {
-            include "pages/account/detail.php";
-        } else if ($_GET['pages'] == 'cetak') {
-            include "pages/account/index.php";
-        } else if ($_GET['pages'] == 'account_custom') {
-            include "pages/account/account_page_custom.php";
+        } else if ($_GET['pages'] == 'kirim') {
+            include "pages/kirim/laporan.php";
 
-        } else if ($_GET['pages'] == 'check_in') {
-            include "pages/check_in/daftar_produk.php";
-        } else if ($_GET['pages'] == 'form_booking') {
-            include "pages/check_in/form_booking.php";
-        } else if ($_GET['pages'] == 'form_checkin') {
-            include "pages/check_in/form_checkin.php";
+        } else if ($_GET['pages'] == 'batal') {
+            include "pages/batal/laporan.php";
 
-        } else if ($_GET['pages'] == 'booking') {
-            include "pages/booking/booking.php";
-        } else if ($_GET['pages'] == 'booking_detail') {
-            include "pages/booking/booking_detail.php";
+        } else if ($_GET['pages'] == 'masuk') {
+            include "pages/masuk/laporan.php";
 
-        } else if ($_GET['pages'] == 'test') {
-            include "pages/booking/test.php";
-
-        } else if ($_GET['pages'] == 'reservasi') {
-            include "pages/reservasi/reservasi.php";        
-        } else if ($_GET['pages'] == 'order_tamu') {
-            include "pages/reservasi/shop.php";        
-        } else if ($_GET['pages'] == 'tambah_order_tamu') {
-            include "pages/reservasi/form_order_tamu.php";        
-        } else if ($_GET['pages'] == 'check_out') {
-            include "pages/reservasi/check_out.php";
+        } else if ($_GET['pages'] == 'pass') {
+            include "pages/password/password.php";
+        } else if ($_GET['pages'] == 'aksi_pass') {
+            include "pages/password/aksi.php";
             
         }  else {
             include "pages/error/403.php";
---
+++ admin/template/header_op.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Plaza Agro | Admin</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="asset/plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="asset/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="asset/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="asset/plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="asset/dist/css/adminlte.min.css">
+  <!-- DataTables -->
+  <link rel="stylesheet" href="asset/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="asset/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="asset/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="asset/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="asset/plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="asset/plugins/summernote/summernote-bs4.min.css">
+</head>
+
+<body class="hold-transition layout-top-nav">
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand-md navbar-light navbar-white">
+        <?php
+          if ($_GET) {
+            $page = $_GET['pages'];
+            }
+            $username = $_SESSION['username'];
+            $query = mysqli_query($koneksi, "SELECT * FROM admins WHERE username='$username'");
+            $user = mysqli_fetch_array($query);
+          
+          
+          ?>
+        <div class="info">
+          <?= $user['nama_lengkap'] ?>
+        </div>
+      <button class="navbar-toggler order-1" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse order-3" id="navbarCollapse">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a href="main.php?pages=home" class="nav-link">HOME</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a id="dropdownSubMenu1" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="nav-link dropdown-toggle">MAIN</a>
+            <ul aria-labelledby="dropdownSubMenu1" class="dropdown-menu border-0 shadow">
+              <li><a href="main.php?pages=masuk&act=" class="dropdown-item">Masuk</a></li>
+              <li><a href="main.php?pages=batal&act=" class="dropdown-item">Batal</a></li>
+              <li class="dropdown-submenu dropdown-hover">
+                <a id="dropdownSubMenu2" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" class="dropdown-item dropdown-toggle">Lunas</a>
+                <ul aria-labelledby="dropdownSubMenu2" class="dropdown-menu border-0 shadow">
+                  <li>
+                    <a tabindex="-1" href="main.php?pages=laporan&act=" class="dropdown-item">Proses</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="main.php?pages=kirim&act=" class="dropdown-item">Kirim</a>
+                  </li>
+                </ul>
+              </li>
+              <li><a href="main.php?pages=testi" class="dropdown-item">Testimoni</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <!-- Right navbar links -->
+      <ul class="order-1 order-md-3 navbar-nav navbar-no-expand ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="main.php?pages=pass">
+             <button class="btn btn-warning">Ganti Pass</button>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="logout.php">
+             <button class="btn btn-danger">Log Out</button>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+            <i class="fas fa-expand-arrows-alt"></i>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button">
+            <i class="fas fa-th-large"></i>
+          </a>
+        </li>
+      </ul>
+  </nav>
+</div>
+</body>
